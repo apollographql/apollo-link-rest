ebdd4d70bfae36333350d1542b80df218c562d6d
Frederic Barthelemy <git@fbartho.com>
2018-09-16 16:00:20 -0700

    Update dependencies

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      babelConfig: false,
+      mapCoverage: true,
+    },
+  },
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  setupFiles: ['./scripts/jest.js'],
+};
